470fb8b3e8cc9086eee97a6a11b18163546c23ba
Emmanuel Schosyman <e.schoysman@gmail.com>
2026-06-15 14:50:25 +0200

    [ADD] WIP - impostazione store

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 'use client';
 import {StrictMode} from "react";
+import { Provider } from 'react-redux'
 
 import {Home} from "@/app/components/home/Home";
+import store from './app/store/store'
 
 const App = () => {
 
   return (
       <StrictMode>
-          <Home/>
+          <Provider store={store}>
+              <Home/>
+          </Provider>
       </StrictMode>
   );
 
---
+++ app/store/store.ts
@@ -0,0 +1,7 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+export default configureStore({
+  reducer: {},
+})
+
+// https://react-redux.js.org/tutorials/quick-start
